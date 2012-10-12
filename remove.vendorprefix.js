4b32a5d500d9a06036b8d4e27ebfc71be69e613c
t.ashula <dev@ashula.info>
2012-10-12 12:50:18 +0900

    comeback -webkit

--- remove.vendorprefix.js
+++ remove.vendorprefix.js
@@ -2,5 +2,5 @@ opera.addEventListener('BeforeCSS', function(userJSEvent){
   userJSEvent.cssText = userJSEvent.cssText
     .replace(/-webkit-gradient\(\s*(linear|radial)\s*,/g, '$1-gradient(')
     .replace(/\(\s*top\s*,/g, '(to bottom,').replace(/\(\s*bottom\s*,/g, '(to top,').replace(/\(\s*left\s*,/g, '(to right,').replace(/\(\s*right\s*,/g, '(to left,')
-    .replace(/-(moz|ms|khtml|webkit|o)-(opacity|column|border|text-overflow|linear-gradient|radial-gradient|transform|transition|animation|keyframes|box-shadow)/g,'$2');
+    .replace(/-(moz|ms|khtml|o)-(opacity|column|border|text-overflow|linear-gradient|radial-gradient|transform|transition|animation|keyframes|box-shadow)/g,'$2');
 }, false);
